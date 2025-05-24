41ededa91429b1670a3848a6e8fe841748f242cc
Ivory Xu <wx566@nyu.edu>
2025-05-23 20:27:58 -0400

    clip editor

---
+++ Assets/IvoryFolder/Scripts/Clip.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2108f5b9d88bae0fe5c63a3371a91dc106e105f81d4c1769db5b917a128c67ab
+size 53
---
+++ Assets/IvoryFolder/Scripts/ClipEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a21619e80cd75f334dac517b0e815abea67d0d73d0acf393766bd70c54464e20
+size 297
