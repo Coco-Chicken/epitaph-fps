ac9628d5ce68c6fbc317b0b34eb6b93430613063
Ivory Xu <wx566@nyu.edu>
2025-05-26 19:45:32 -0400

    bullet structure

---
+++ Assets/IvoryFolder/Scripts/BulletEffect.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0cbd3fc49dd81616c6386a8152f362fa7db90074c16b658d842ed00193a4421
+size 223
---
+++ Assets/IvoryFolder/Scripts/ClipConfig.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:654cd1b50ac8344d5a1b43130d5f03f13c220a8c59ea188dd4b4a5792be35ec1
+size 166
