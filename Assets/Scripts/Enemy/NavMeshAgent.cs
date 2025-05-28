84c00d5867893ca9117cda619057a97015731f37
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-05-28 15:14:49 -0700

    ECS Enemy follow implementation

---
+++ Assets/Scripts/Enemy/NavMeshAgent.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85ad39fb3a4735ef9fd119b80b26b3fb4f3cde7157cb791ef67779622b99efb9
+size 2722
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d30e226906940934043fcc0cf2228c26ec8f9c971280e6eaff49b9e639c4d44
+size 966
