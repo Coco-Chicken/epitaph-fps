667d6d7449732785b779a87867d0e509e523db48
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-06-15 17:52:12 -0700

    StateMachine

---
+++ Assets/Scripts/Enemy/ZombieController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5da81d6732ce76dac3a381f669ac5bfae704e106f3aa064d55cdfe6f2ae0bac
+size 1108
---
+++ Assets/Scripts/StateMachine/IState.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3873a3721f0fbc5826b3a7eca945145a4fee8ccc860fefa1cd3ef0a1f8f66840
+size 246
